b9866a10e99f6861128763045dffbee086a7cd2c
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2021-05-02 21:11:40 +0200

    poc to loan abook with barcode scanner ok!

---
+++ app/javascript/controllers/barcode_scanner_controller.js
@@ -0,0 +1,47 @@
+import { Controller } from "stimulus";
+import Quagga from 'quagga/dist/quagga';
+
+export default class extends Controller {
+  static targets = [ "camArea", "form", "input" ]
+
+  connect() {
+    Quagga.init({
+      inputStream : {
+        name : "Live",
+        type : "LiveStream",
+        numOfWorkers: navigator.hardwareConcurrency,
+        target: this.camAreaTarget
+      },
+      decoder : {
+        readers : [
+          "code_128_reader",
+          "ean_reader",
+          "ean_8_reader",
+          "code_39_reader",
+          "code_39_vin_reader",
+          "codabar_reader",
+          "upc_reader",
+          "upc_e_reader",
+          "i2of5_reader",
+          "2of5_reader",
+          "code_93_reader"
+        ]
+      }
+    }, (err) => {
+        if (err) {
+            console.log(err);
+            return
+        }
+        console.log("Initialization finished. Ready to start");
+
+        Quagga.onDetected((data) => {
+          if (data.codeResult.code.length > 12) {
+            this.inputTarget.value = data.codeResult.code;
+            Quagga.stop();
+            this.formTarget.submit();
+          }
+        });
+        Quagga.start();
+    });
+  }
+}
