e4431bd06de339328a761d256af4b89649a47584
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2021-05-02 19:41:25 +0200

    ISBN to book data

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -6,9 +6,15 @@ class LoansController < ApplicationController
   end
 
   def new
+    isbn = session[:loan]["isbn"]
+    response = HTTParty.get("http://openlibrary.org/api/volumes/brief/isbn/#{isbn}.json")
+    book = JSON.parse(response.body, symbolize_names: true)
+
     @loan = Loan.new(
       borrower_id: session[:loan]["borrower_id"],
-      isbn: session[:loan]["isbn"]
+      isbn: isbn,
+      title: book[:records].values.first[:data][:title],
+      author: book[:records].values.first[:data][:authors].map { |author| author[:name] }.to_sentence
     )
   end
 
@@ -24,6 +30,6 @@ class LoansController < ApplicationController
   private
 
   def loan_params
-    params.require(:loan).permit(:borrower_id, :isbn)
+    params.require(:loan).permit(:borrower_id, :isbn, :title, :author)
   end
 end
