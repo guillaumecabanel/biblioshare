99ad0bae21dd1c219d4dfc9b6aff6939185d7046
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2021-05-14 16:42:40 +0200

    Add initials method to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :loaned_books, foreign_key: :lender_id, class_name: "Loan"
   has_many :borrowed_books, foreign_key: :borrower_id, class_name: "Loan"
+
+  def initials
+    full_name.split[0..1].map(&:first).join.upcase
+  end
 end
